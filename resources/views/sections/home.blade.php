c9e9522a1a5d3c1c5430087673ea07c31796cf09
hanif077 <mdabuhanif007@gmail.com>
2019-11-24 18:45:33 +0600

    sorry for late

---
+++ resources/views/sections/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+{{header('Content-Type: text/html; charset=utf-8')}}
+
+@section('content')
+
+<h1>Form submission</h1>
+<p>Framework: <b>Laravel</b></p>
+<p>Library: <b>bootstrap, jquery</b></p>
+
+
+@endsection
+
+
+
+
